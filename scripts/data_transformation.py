7cf88492068161443eb7d962a7c94106dc2fd275
Michael Tamirie <michaeltamirie@gmail.com>
2022-05-27 16:56:00 +0300

    Edit the script

--- scripts/data_transformation.py
+++ scripts/data_transformation.py
@@ -1,3 +1,4 @@
+import imp
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
@@ -5,19 +6,17 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import FunctionTransformer
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+from scripts.data_cleaning import logger
 
 class DataTransformer:
     """
     provides transformer functions for machine learning.
     """
-    def __init__(self, filehandler) -> None:
-        file_handler = logging.FileHandler(filehandler)
-        formatter = logging.Formatter("time: %(asctime)s, function: %(funcName)s, module: %(name)s, message: %(message)s \n")
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+    def __init__(self) -> None:
+        pass
+
+        logger.info('Function DataTransformer successfully added!!!')
+        
 
 
     def sep_cat_num(self, df):
@@ -27,23 +26,24 @@ class DataTransformer:
         categorical_columns = df.select_dtypes(include='object')
         numerical_columns = df.select_dtypes(exclude='object')
 
-        logger.info("successfully separated")
-        
+        logger.info('Separating catagorical and numerical variables successfuly done!!!')
+
         return categorical_columns, numerical_columns
+
 
     
     def cat_labeler(self, df, cat_cols):
         """
         assigns a numerical label to categorical values
         """
-        try:
-            for column in cat_cols:
-                encoder = LabelEncoder()
-                df[column] = encoder.fit_transform(df[column])
+        for column in cat_cols:
+            encoder = LabelEncoder()
+            df[column] = encoder.fit_transform(df[column])
         
-            print("catagories successfully labeled")
-        except:
-            return df
+        print("catagories successfully labeled")
+
+        logger.info('Labeling the catagorical columns using label encoder successfuly done!!!')
+
         return df
 
 
@@ -55,7 +55,8 @@ class DataTransformer:
         df[:] = scaling.fit_transform(df[:])
 
         print("Data successfully scaled")
-        logger.info("Dataset successfully scaled")
+
+        logger.info('Scaling the data successfuly done!!!')
         
         return df
 
@@ -69,22 +70,21 @@ class DataTransformer:
         scaled = pd.DataFrame(scaler.fit_transform(df))
 
         print("Data successfully normalized")
-        logger.info("Dataset successfully normalized")
+
+        logger.info('Normalizing the data successfuly done!!!')
 
         return scaled
 
-    def target_feature(self, df, t):
+    def target_feature(self, df, f_r, t):
         """
         target and feature separator
-        f: starting index for features
-        t: the index of the target varoab;e
         """
-
-        features = (df.drop(df.columns[[t]], axis = 1)).values
+        features = df.iloc[:,f_r[0]:f_r[1]].values
         target = df.iloc[:,t].values
         
         print("target and features separated")
-        logger.info("target and features separated")
+
+        logger.info('Separating target features successfuly done!!!')
 
         return features, target
 
@@ -99,7 +99,7 @@ class DataTransformer:
         x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,test_size= per_2, shuffle = True, random_state = rand_state)
 
         print("data successfully splitted")
-        logger.info("data successfully splitted")
 
+        logger.info('Splitting successfuly done!!!')
 
         return [x_train, y_train, x_test, y_test, x_val, y_val]
