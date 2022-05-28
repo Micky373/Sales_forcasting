6d0f9123ae456470c2e5abb95c0fe0b1beb83d23
Michael Tamirie <michaeltamirie@gmail.com>
2022-05-28 14:02:30 +0300

    Finish sampling the data

---
+++ apps/prediction_display.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def app():
+    with st.spinner("Loading Home ..."):
+        
+        st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:1.8rem;border-radius:0.8rem;'>Rossmann Pharmaceuticals</p>", unsafe_allow_html=True)
+    
+        st.write(
+            """
+            Dirk Rossmann GmbH (usual: Rossmann) is one of the largest drug store chains in Europe with around 56,200 employees and more than 4000 stores across Europe.
+                """
+        )
+        st.write("""
+        
+         This app is an end-to-end solution that allows the Rosemann pharmaceutical company to see sales forecasts for its stores six weeks in advance, as well as expected trends.
+         """)
