e9618f488ec49fae6ef1c2a012fa7516776005b9
Michael Tamirie <michaeltamirie@gmail.com>
2022-05-28 15:51:35 +0300

    Edit dataset app

--- apps/dataset_display.py
+++ apps/dataset_display.py
@@ -7,9 +7,9 @@ def app():
     with st.spinner("Loading Data ..."):
         st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:2rem;border-radius:0.8rem;'>Data display page</p>", unsafe_allow_html=True)
      
-        st.markdown("<p style='font-size:1.8rem'>The data contains historical sales data for 1,115 Rossmann stores.</p>", unsafe_allow_html=True)
+        st.markdown("<p style='font-size:1.8rem'>The report includes sales statistics from 1,115 Rossmann outlets over the years.</p>", unsafe_allow_html=True)
 
-        st.markdown("<p style='font-size:1.8rem'>The data and feature description for this challenge can be found https://www.kaggle.com/c/rossmann-store-sales</p>",unsafe_allow_html=True)
+        st.markdown("<p style='font-size:1.8rem'>This challenge's data and feature description can be found at https://www.kaggle.com/c/rossmann-store-sales.</p>",unsafe_allow_html=True)
  
 
         train = pd.read_csv('./data/train_display.csv')
